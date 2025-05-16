ce5273f3a6514aa69919865678c3db1d8a1c6e06
Samy RABAH-MONTAROU <samy.rabah-montarou@ledger.fr>
2025-05-16 17:50:39 +0200

    feat: read file, sample file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     // let args: Vec<std::ffi::OsString> = std::env::args_os().collect();
@@ -11,4 +12,8 @@ fn main() {
 
     println!("Searching for {query}");
     println!("In file {file_path}");
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
