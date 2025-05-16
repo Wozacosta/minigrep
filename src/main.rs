84d470f98388a5e931bdb18e5baf155fe0545c97
Samy RABAH-MONTAROU <samy.rabah-montarou@ledger.fr>
2025-05-16 17:46:10 +0200

    feat: accepting cli args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // let args: Vec<std::ffi::OsString> = std::env::args_os().collect();
+    // dbg!(args);
+    let args: Vec<String> = env::args().collect();
+    dbg!(&args);
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {query}");
+    println!("In file {file_path}");
 }
