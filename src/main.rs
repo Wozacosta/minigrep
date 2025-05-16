d22f3eb02233384c6b3b69697e1f797fd94afa16
Samy RABAH-MONTAROU <samy.rabah-montarou@ledger.fr>
2025-05-17 00:32:59 +0200

    feat: write errors to stderr

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,15 @@ use std::env;
 use std::process;
 
 fn main() {
-    // let args: Vec<std::ffi::OsString> = std::env::args_os().collect();
-    // dbg!(args);
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
